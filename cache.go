e81ab309feaf8a2280e5852e97df077457b52755
Igor Yagodin <Igor.Yagodin@x5.ru>
2022-06-19 14:08:36 +0300

    init

--- cache.go
+++ cache.go
@@ -2,22 +2,43 @@ package cache
 
 import "time"
 
+type Pair struct {
+	Value    string
+	Deadline time.Time
+}
+
+func NewPair(value string, deadline time.Time) Pair {
+	return Pair{Value: value, Deadline: deadline}
+}
+
 type Cache struct {
+	data map[string]Pair
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{data: make(map[string]Pair)}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (C Cache) Get(key string) (string, bool) {
 
+	a, b := C.data[key]
+
+	return a.Value, b
 }
 
-func (receiver) Put(key, value string) {
+func (C Cache) Put(key, value string) {
+	C.data[key] = NewPair(value, time.Time{})
 }
 
-func (receiver) Keys() []string {
+func (C Cache) Keys() []string {
+	mymap := C.data
+	keys := make([]string, 0, len(mymap))
+	for k := range mymap {
+		keys = append(keys, k)
+	}
+
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (C Cache) PutTill(key, value string, deadline time.Time) {
 }
